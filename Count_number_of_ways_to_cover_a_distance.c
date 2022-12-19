50c3901180432cb78ca57e6e6213f2ec02cb50f3
BhagyasRii <119729822+BhagyasRii@users.noreply.github.com>
2022-12-19 14:55:51 +0530

    #include <stdio.h> int cls(int k) {    if(k<0)    {        return 0;    }    if(k==0)    {        return 1;    } return cls(k-1)+cls(k-2)+cls(k-3); } int main() {     int r,p;     scanf("%d",&r);     p=cls(r);     printf("%d",p); }

---
+++ Count_number_of_ways_to_cover_a_distance.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int cls(int k)
+{
+   if(k<0)
+   {
+       return 0;
+   }
+   if(k==0)
+   {
+       return 1;
+   }
+return cls(k-1)+cls(k-2)+cls(k-3);
+}
+int main()
+{
+    int r,p;
+    scanf("%d",&r);
+    p=cls(r);
+    printf("%d",p);
+}
