66e0a719635e9b3601d65f4b23cb573325b7120c
BhagyasRii <119729822+BhagyasRii@users.noreply.github.com>
2023-08-09 14:09:06 +0530

    #include <stdio.h> int main() {     int n,m,l;     scanf("%d%d",&n,&m);     for(int i=1;i<=m;i++){         if(m%i==0 && n%i==0)         {             l=i;         }     }     int lcm=(m*n)/l;     printf("%d",lcm); }

--- LCM.c
+++ LCM.c
@@ -1,16 +1,14 @@
 #include <stdio.h>
-#include <math.h>
 int main()
 {
-    int a,b,n,i,l,g;
-    scanf("%d%d",&a,&b);
-    for(i=1;i<=a&&i<=b;i++)
-    {
-        if(a%i==0&&b%i==0)
+    int n,m,l;
+    scanf("%d%d",&n,&m);
+    for(int i=1;i<=m;i++){
+        if(m%i==0 && n%i==0)
         {
-            g=i;
+            l=i;
         }
     }
-    l=(a*b)/g;
-    printf("%d",l);
+    int lcm=(m*n)/l;
+    printf("%d",lcm);
 }
